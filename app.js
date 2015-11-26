c630a5154c4604e02a86639069763434e0c73832
marie <marie.sochala@gmail.com>
2015-11-26 18:59:28 +0100

    Cours Javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+function CompareChaines(str){
+	var maj = str.toUpperCase();
+
+	if (str==maj){
+	document.writeln("c'est parfait, c'est exact</br>");
+}
+else {
+	document.writeln("ce n'est pas pareil</br>");
+}
+}
+var foo ="qljhcbzeend";
+
+CompareChaines(foo);
+CompareChaines("MDEEONDOZEND");
+
+
+
